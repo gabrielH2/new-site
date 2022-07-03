98d9cb0790a16faf4d33cff7db454a38f0a62fc4
gabrielH2 <gabrielmachado358@gmail.com>
2022-07-02 23:00:30 -0300

    first commit

---
+++ ing.js
@@ -0,0 +1,23 @@
+alert("Olá, Seja bem vindo ao site MyLife!!!")
+
+function idade(){
+     alert("Tenho 19 anossss")
+}
+function nome(){
+    alert("Me chamo Gabriel Machado ")
+}
+function formacao(){
+    alert("Atualmente estou cursando o 3º periodo de sistemas para internet na Universidade católica de Pernambuco")
+}
+function vivo(){
+    alert("Moro em Recife, a maior capital em linha curva do mundo, hahaha")
+}
+function hob(){
+    alert("Nas horas vagas pratico ciclismo, geralmente vou a praia de bike aos domingos")
+}
+function Job(){
+    alert("trabalho na Universidade Católica na aréa de Suporte de TI")
+}
+function con(){
+    alert("email: gabrielmachado358@gmail.com "+" / "+ "número: 81 983695961")
+}
